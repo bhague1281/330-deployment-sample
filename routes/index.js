6c5ee0ce876183fe51f7b324c510833ce0de0c61
Brian Hague <bhague1281@gmail.com>
2025-05-13 20:13:21 -0700

    add static file serving

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
-const { Router } = require("express");
-const router = Router();
+const express = require("express");
+const router = express.Router();
 
 router.use("/items", require('./items'));
-router.get("/", (req, res, next) => {
-  res.send(`
-    <html>
-      <body>
-        <h1> Hello, world! </h1>
-        <p>Welcome to the Express server!</p>
-      </body>
-    </html>
-  `)
-});
+
+console.log(__dirname + '/../deployment-sample-frontend/dist');
+router.use(express.static(__dirname + '/../deployment-sample-frontend/dist'));
+
+// router.get("/", (req, res, next) => {
+//   res.send(`
+//     <html>
+//       <body>
+//         <h1> Hello, world! </h1>
+//         <p>Welcome to the Express server!</p>
+//       </body>
+//     </html>
+//   `)
+// });
 
 module.exports = router;
